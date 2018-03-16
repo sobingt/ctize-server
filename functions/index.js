5a7bc9d5f0c1bd8e643cd41c4ebe00fc3c81fdfb
drishit96 <drishitmitra@gmail.com>
2018-03-16 22:40:23 +0530

    Private messaging notification (Changes made by Gaurav)

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,20 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
-// The Firebase Admin SDK to access the Firebase Realtime Database. 
+// The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 let communityId;
+let receiverId;
+let message;
+let sendId;
 let commentId;
 let postId;
 let type;
 let topic;
 let payload;
+let token;
 
 exports.notifyComment = functions.https.onRequest((request, response) => {
 	communityId = request.body.communityId;
@@ -20,6 +24,16 @@ exports.notifyComment = functions.https.onRequest((request, response) => {
 	sendNotification(type);
 	response.status(200).send(postId);
 });
+
+exports.notifyChat = functions.https.onRequest((request, response) => {
+	sendId = request.body.sendId;
+	receiverId = receiverId.body.receiverId;
+	message = request.body.message;
+	type = "message";
+	sendNotification(type);
+	response.status(200).send(postId);
+});
+
 
 function sendNotification(type) {
 	//send notification only if the id exists in database
@@ -77,4 +91,35 @@ function sendNotification(type) {
             }
         });
     }
+
+		if (type === "message") {
+
+			let userRef = admin.database().ref("user_details").child(sendId);
+			userRef.once("value", function(snapshot) {
+					let user = snapshot.val();
+					token = String(user.token);
+					payload = {
+							//notification: {
+							//    title: String(user.userName).concat(" has commented"),
+							//    body: String(commentData.comment)
+							//},
+							data: {
+									type: "message",
+									from: String(user.userName),
+									message: String(message)
+							}
+					};
+
+					admin.messaging().sendToDevice(token, payload)
+			  .then(function(response) {
+			    // See the MessagingDevicesResponse reference documentation for
+			    // the contents of response.
+					console.log(payload);
+			    console.log("Successfully sent message:", response);
+			  })
+			  .catch(function(error) {
+			    console.log("Error sending message:", error);
+			  });
+		});
+  }
 }
